581866b661ff09c29798385a298e5529d268bad2
Emmanuel <redmanuel247@gmail.com>
2021-05-27 12:16:43 -0400

    migrated db from sqlite to mysql

--- inventory_project/inventory_project/urls.py
+++ inventory_project/inventory_project/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
- + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
